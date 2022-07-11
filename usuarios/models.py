2e60934d490eeab8fa7399b4f3df5b9b2fd70b74
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2022-07-11 13:08:39 -0300

    fix: fixed 'password' to 'senha' and added 'ativo' field

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Usuario(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+    senha = models.CharField(max_length=255)
+    ativo = models.BooleanField(default=True)
