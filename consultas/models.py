c8a94c40a1244665c3a2aa2f5173373524910907
Yorran <yorranjrrigatti@gmail.com>
2022-07-11 12:57:18 -0300

    feat: updated consulta model

--- consultas/models.py
+++ consultas/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Consulta(models.Model):
     id         = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
 
-    paciente   = models.ForeignKey('pacientes.Paciente', related_name='consulta', on_delete=models.CASCADE)
+    paciente   = models.ForeignKey('pacientes.Paciente', related_name='consulta', null=True, on_delete=models.CASCADE)
     convenio   = models.OneToOneField('convenios.Convenio', related_name='consulta', on_delete=models.CASCADE)
     atendente  = models.ForeignKey('atendentes.Atendente', related_name='consulta', on_delete=models.CASCADE)
     medico     = models.ForeignKey('medicos.Medico', related_name='consulta', on_delete=models.CASCADE)
