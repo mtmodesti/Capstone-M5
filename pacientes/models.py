ccc17295bd12a51dd2a4df88d89e0b3e19ea4050
Christian <christianbm1998@gmail.com>
2022-07-12 19:00:02 -0400

    feat: create views and urls to pacientes

--- pacientes/models.py
+++ pacientes/models.py
@@ -12,3 +12,8 @@ class Paciente(models.Model):
     telefone = models.CharField(max_length=30)
     data_nascimento = models.DateTimeField()
     data_cadastro = models.DateTimeField()
+    ativo = models.BooleanField(default=True)
+
+    convenio = models.OneToOneField(
+        "convenios.Convenio", models.SET_NULL, related_name="paciente", null=True
+    )
