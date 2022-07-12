e7632b9713f53aeb237dead79f3fb85b302cac53
Christian <christianbm1998@gmail.com>
2022-07-11 20:04:17 -0400

    feat: create model paciente

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,14 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Paciente(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=11, unique=True)
+    telefone = models.CharField()
+    data_nascimento = models.DateTimeField()
+    data_cadastro = models.DateTimeField()
