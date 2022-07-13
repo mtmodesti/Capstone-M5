5e123866e1fb4f08d207172c80be0b7456f9cef6
Christian <christianbm1998@gmail.com>
2022-07-13 10:32:38 -0400

    feat: add permision in agenda views

--- agendas/views.py
+++ agendas/views.py
@@ -1,15 +1,20 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Agenda
+from .permissions import isSuperUserOrStaffOrOwner
 from .serializers import AgendaSerializer
 
 
 class ListCreateAgendaView(ListCreateAPIView):
+    pagination_class = [isSuperUserOrStaffOrOwner]
+
     queryset = Agenda.objects.all()
     serializer_class = AgendaSerializer
 
 
 class RetrieveUpdateDestroyAgendaView(RetrieveUpdateDestroyAPIView):
+    pagination_class = [isSuperUserOrStaffOrOwner]
+
     queryset = Agenda.objects.all()
     serializer_class = AgendaSerializer
 
