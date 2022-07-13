fc940e7cf94c1196d1e08556bf60b4b6bd9990de
Gabriel Rocha <gabrielrs7@outlook.com>
2022-07-13 10:05:50 -0300

    refactor: changing Medico model

--- medicos/models.py
+++ medicos/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 import uuid
 from agendas.models import Agenda
-
+from usuarios.models import Usuario
 
 
 class Medico(models.Model):
 
     id            = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    nome          = models.CharField(max_length=127)
-    password      = models.CharField(max_length=127)
     especialidade = models.CharField(max_length=127)
     telefone      = models.CharField(max_length=127)
-    email         = models.CharField(max_length=127)
     ativo         = models.BooleanField(default=True)
-
-    agenda = models.ForeignKey(Agenda, on_delete=models.CASCADE, default = None)
+    usuario       = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name="medico")
+    agenda        = models.ForeignKey(Agenda, on_delete=models.CASCADE, default = None)
