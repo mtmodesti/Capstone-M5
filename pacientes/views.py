a91e438ebd82c056a64d33aec9a562e5353eabee
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2022-07-18 13:33:47 -0300

    feat: created filter for pacientes convenio type and anamneses by paciente

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Paciente
@@ -10,6 +11,17 @@ class ListCreatePacienteView(ListCreateAPIView):
 
     queryset = Paciente.objects.all()
     serializer_class = PacienteSerializer
+    convenio_separator = ","
+
+    def get_queryset(self):
+        convenios = self.request.query_params.get("convenio", None)
+        if convenios:
+            qs = Paciente.objects.all()
+            for convenio in convenios.split(self.convenio_separator):
+                qs = qs.filter(convenio__tipo=convenio)
+            return qs
+
+        return super().get_queryset()
 
 
 class RetrieveUpdateDestroyPacienteView(RetrieveUpdateDestroyAPIView):
