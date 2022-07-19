4c65c3ac8925ebf7df1200009c5bfbdca3ed2d36
Gabriel Rocha <gabrielrs7@outlook.com>
2022-07-19 13:58:21 -0300

    fix: adding uuid to url to avoid 500 error

--- anamneses/urls.py
+++ anamneses/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("anamneses/", views.ListAnamneseView.as_view()),
-    path("anamneses/criar/<paciente_id>/", views.CreateAnamneseView.as_view()),
+    path("anamneses/criar/<uuid:paciente_id>/", views.CreateAnamneseView.as_view()),
     path("anamneses/<pk>/", views.RetrieveUpdateDestroyAnamneseView.as_view()),
 ]
